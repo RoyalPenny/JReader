22d0f4486462456cdc3468bbe94a2c9955b8c4a8
RoyalPenny <100495516+RoyalPenny@users.noreply.github.com>
2025-03-18 15:56:46 +1300

    Squashed commit of the following:
    commit 7df22565c0ab0a591458c6e72255ffea44c99ab9
    Author: RoyalPenny <100495516+RoyalPenny@users.noreply.github.com>
    Date:   Tue Mar 18 13:43:01 2025 +1300
    
        implemented change voice function
    
    commit f253a15a64df6d4f03aa5e1c28367e720e838d32
    Author: RoyalPenny <100495516+RoyalPenny@users.noreply.github.com>
    Date:   Tue Mar 18 13:37:51 2025 +1300
    
        refactored hashmaps

--- app/src/main/java/jreader/QuoteMap.java
+++ app/src/main/java/jreader/QuoteMap.java
@@ -9,12 +9,12 @@ import com.microsoft.cognitiveservices.speech.VoiceInfo;
 
 public class QuoteMap {
     private HashMap<String, List<Integer>> quotes;
-    private HashMap<List<Integer>, VoiceInfo> voices;
+    private HashMap<List<Integer>, String> associatedEntity;
     private final HashMap<String, VoiceInfo> entites;
 
     public QuoteMap() {
         this.quotes = new HashMap<>();
-        this.voices = new HashMap<>();
+        this.associatedEntity = new HashMap<>();
         this.entites = new HashMap<>();
     }
 
@@ -22,8 +22,8 @@ public class QuoteMap {
         return this.quotes;
     }
 
-    public HashMap<List<Integer>, VoiceInfo> getVoicesHashMap() {
-        return this.voices;
+    public HashMap<List<Integer>, String> getassociatedEntityHashMap() {
+        return this.associatedEntity;
     }
 
     public HashMap<String, VoiceInfo> getEntitiesHashMap() {
@@ -34,12 +34,12 @@ public class QuoteMap {
         this.quotes.put(quote.trim(), sentenceIndices);
     }
 
-    public void addVoice(List<Integer> sentenceIndices, VoiceInfo voice) {
-        this.voices.put(sentenceIndices, voice);
+    public void addAssociatedEntity(List<Integer> sentenceIndices, String entity) {
+        this.associatedEntity.put(sentenceIndices, entity);
     }
 
-    public void addEntity(String chain, VoiceInfo voice) {
-        this.entites.put(chain, voice);
+    public void addEntity(String entity, VoiceInfo voice) {
+        this.entites.put(entity, voice);
     }
 
     public List<Integer> getQuoteToIndex(String quote) {
@@ -47,20 +47,24 @@ public class QuoteMap {
         return this.quotes.get(quote);
     }
 
-    public VoiceInfo getIndexToVoice(List<Integer> index) {
-        return this.voices.get(index);
+    public String getIndexToAssocciatedEntity(List<Integer> index) {
+        return this.associatedEntity.get(index);
     }
 
-    public VoiceInfo getEntityToVoice(String chain) {
-        return this.entites.get(chain);
+    public VoiceInfo getEntityToVoice(String entity) {
+        return this.entites.get(entity);
     }
 
-    public void clearVoices() {
-        this.voices = new HashMap<>();
+    public void clearAssociatedEntity() {
+        this.associatedEntity = new HashMap<>();
     }
 
     public void clearQuotes() {
         this.quotes = new HashMap<>();
+    }
+
+    public void changeVoice(String entity, VoiceInfo voice) {
+        this.entites.put(entity, voice);
     }
 
     public List<Map.Entry<String, List<Integer>>> getAllQuoteEntries() {
